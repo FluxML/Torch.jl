2dcc5dc201a1cc67e263d6cb32e8e9ee9b61c989
Dhairya Gandhi <dhairya@juliacomputing.com>
2020-02-04 15:30:42 +0530

    rm PATH

--- src/Torch.jl
+++ src/Torch.jl
@@ -5,8 +5,6 @@ export Tensor, tensor
 using YAML
 using Clang
 
-const PATH = "/home/dhairyagandhi96/temp/aten/torch_cpp/ocaml-torch/src/wrapper" # /build" # /libdoeye_caml.so"
-
 # include("wrap2.jl")
 
 include("libtorch_common.jl")
