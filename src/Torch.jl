949d7d4a025231338b0f93a1dca1f1c6114ab754
Dhairya Gandhi <dhairya@juliacopmuting.com>
2020-02-12 21:08:46 +0530

    sync utils

--- src/Torch.jl
+++ src/Torch.jl
@@ -12,6 +12,10 @@ TURN_ON_LOGGING = false
 include("wrap/libtorch_common.jl")
 include("wrap/libdoeye_caml_generated.jl")
 
+# sync + clear empty cache
+const clear_cache = at_empty_cache
+const sync = at_sync
+
 include("tensor.jl")
 include("scalar.jl")
 include("ops.jl")
--- src/tensor.jl
+++ src/tensor.jl
@@ -20,8 +20,6 @@ function no_grad(f; flag = 0)
   f()
 end
 
-sync() = at_sync()
-
 async_free!(x) = let x = x, ptr = x.ptr, oid = objectid(x)
   @async begin
     free!(x)
