c8f1e9c71d7a41a3193c5d9fec3ef88e4737b593
Jesper Stemann Andersen <jsa@ihp.dk>
2023-08-11 16:47:49 +0200

    Dev. container: Added support for CUDA and CUDNN
    Also:
    * Dev. container: Updated for Torch 1.10.2
    * Added /build to .gitignore

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,27 @@
+sudo chown -R vscode:vscode /opt/juliaup /opt/julia_depot
+
+CUDA_VERSION_MAJOR_MINOR=$(echo $CUDA_VERSION | cut -d . -f 1-2)
+TMP_PROJECT=$(mktemp -d)
+cd $TMP_PROJECT
+touch Project.toml
+cat <<EOT > LocalPreferences.toml
+[CUDA_Runtime_jll]
+version = "$CUDA_VERSION_MAJOR_MINOR"
+EOT
+CUDNN_ROOT=$(julia --project --eval '
+        using Pkg;
+        CUDA_VERSION = VersionNumber(ENV["CUDA_VERSION"])
+        if CUDA_VERSION < v"11"
+            Pkg.add(name="CUDA_Runtime_jll", version="0.2")
+        elseif CUDA_VERSION < v"11.4"
+            Pkg.add(name="CUDA_Runtime_jll", version="0.7")
+        else
+            Pkg.add(name="CUDA_Runtime_jll")
+        end
+        Pkg.add(name="CUDNN_jll", version=ENV["CUDNN_VERSION"]);
+        using CUDNN_jll;
+        println(CUDNN_jll.artifact_dir)') \
+&& for F in $CUDNN_ROOT/include/cudnn*.h; do ln -s $F /usr/local/cuda/include/$(basename $F); done \
+&& for F in $CUDNN_ROOT/lib/libcudnn*; do ln -s $F /usr/local/cuda/lib64/$(basename $F); done
+
+opam init --disable-sandboxing --auto-setup
