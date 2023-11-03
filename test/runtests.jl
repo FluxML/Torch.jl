4ac15ab4eeb734a6245dc992e41970c42a1ca5e6
Jesper Stemann Andersen <jsa@ihp.dk>
2023-11-03 04:11:51 +0100

    Re-organized tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,31 +1,7 @@
-using Test, Flux, Metalhead
-using Torch
-using Torch: Tensor, tensor
+using Test
 
-@testset "Movement" begin
-  r = rand(Float32, 3,3)
-  tr = tensor(r, dev = 0)
-  @test tr isa Tensor
-  @test tr .* tr isa Tensor
-
-  cr = collect(tr)
-  @test cr isa Array
+@testset verbose=true "Torch" begin
+    include("flux_tests.jl")
+    include("tensor_movement_tests.jl")
+    include("tensor_nnlib_tests.jl")
 end
-
-@testset "Flux" begin
-  resnet = ResNet()
-  tresnet = Flux.fmap(Torch.to_tensor, resnet.layers)
-
-  ip = rand(Float32, 224, 224, 3, 1) # An RGB Image
-  tip = tensor(ip, dev = 0) # 0 => GPU:0 in Torch
-
-  top = tresnet(tip)
-  op = resnet.layers(ip)
-
-  gs = gradient(() -> sum(tresnet(tip)), Flux.params(tresnet))
-  @test top isa Tensor
-  @test size(top) == size(op)
-  @test gs isa Flux.Zygote.Grads
-end
-
-include("test_nnlib.jl")
